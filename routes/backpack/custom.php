2dd3b9ecc2ec16600f2f3349ad680c94a2055d3e
medetmuratbek <muratbek_05@inbox.ru>
2026-04-08 23:28:11 +0500

    BBS-2:added admin panel for models

---
+++ routes/backpack/custom.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'prefix'     => config('backpack.base.route_prefix', 'admin'),
+    'middleware' => array_merge(
+        (array)config('backpack.base.web_middleware', 'web'),
+        (array)config('backpack.base.middleware_key', 'admin')
+    ),
+    'namespace'  => 'App\Http\Controllers\Admin',
+], function () {
+    Route::crud('barbershop', 'BarbershopCrudController');
+    Route::crud('service-category', 'ServiceCategoryCrudController');
+    Route::crud('service', 'ServiceCrudController');
+});
