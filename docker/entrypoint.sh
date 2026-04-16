a98d121b83b3900fdf429966e1cfb9c164849ed5
medetmuratbek <muratbek_05@inbox.ru>
2026-04-16 18:51:23 +0500

    main:fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 set -e
 
-# Set default PORT if not provided
-export PORT=${PORT:-8080}
-
-# Substitute $PORT in nginx config
-envsubst '${PORT}' < /etc/nginx/http.d/default.conf > /etc/nginx/http.d/default.conf.tmp
-mv /etc/nginx/http.d/default.conf.tmp /etc/nginx/http.d/default.conf
-
 php artisan migrate --force
 php artisan storage:link 2>/dev/null || true
 php artisan l5-swagger:generate 2>/dev/null || true
