c1d345b7be50a51197db5c2e34292ded84f78234
medetmuratbek <muratbek_05@inbox.ru>
2026-04-17 17:13:45 +0500

    main:fix assets

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,5 +4,7 @@ set -e
 php artisan migrate --force
 php artisan storage:link 2>/dev/null || true
 php artisan l5-swagger:generate 2>/dev/null || true
+php artisan basset:clear 2>/dev/null || true
+php artisan basset:cache 2>/dev/null || true
 
 exec /usr/bin/supervisord -c /etc/supervisord.conf
