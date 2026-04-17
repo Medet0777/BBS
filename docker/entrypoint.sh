c1232672929be25cf8daca54e68138cb1a8a9648
medetmuratbek <muratbek_05@inbox.ru>
2026-04-17 17:43:37 +0500

    fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 php artisan migrate --force
 php artisan storage:link 2>/dev/null || true
 php artisan l5-swagger:generate 2>/dev/null || true
+rm -rf /var/www/html/storage/app/public/basset 2>/dev/null || true
 php artisan basset:clear 2>/dev/null || true
 php artisan basset:cache 2>/dev/null || true
 
