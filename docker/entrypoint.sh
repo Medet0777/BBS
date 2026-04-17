02f3e3b3929d816e5cbb8bc2b1bde3ec080a1590
medetmuratbek <muratbek_05@inbox.ru>
2026-04-17 18:04:42 +0500

    main:fiiix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
 
+mkdir -p /var/www/html/storage/app/public/basset
+chmod -R 777 /var/www/html/storage /var/www/html/bootstrap/cache
+
 php artisan migrate --force
 php artisan storage:link 2>/dev/null || true
 php artisan l5-swagger:generate 2>/dev/null || true
-rm -rf /var/www/html/storage/app/public/basset 2>/dev/null || true
+rm -rf /var/www/html/storage/app/public/basset/* 2>/dev/null || true
 php artisan basset:clear 2>/dev/null || true
 php artisan basset:cache 2>/dev/null || true
 
